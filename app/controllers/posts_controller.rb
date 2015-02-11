544c003f5e733a358e94aa792aa857a16e17498f
son <son@lincolnix.net>
2015-02-11 19:08:17 +0000

    assets

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,14 +24,12 @@ class PostsController < ApplicationController
   def create
     @post = Post.new(post_params)
 
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+      #render :show, status: :created, location: @post 
+    else
+      render :new 
+      render json: @post.errors, status: :unprocessable_entity 
     end
   end
 
