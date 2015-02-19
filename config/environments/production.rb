dee396f266240a85d8a279fd737c61920daa384a
son <son@lincolnix.net>
2015-02-19 20:53:21 +0000

    minor

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,12 +14,12 @@ Ransom::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  #Rails.application.routes.default_url_options[:host] = 'http://www.5piecetheory.com'
+  Rails.application.routes.default_url_options[:host] = '5piecetheory.com'
   
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  config.action_dispatch.rack_cache = true
+  config.action_dispatch.rack_cache = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
